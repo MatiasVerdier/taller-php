065b0c2f895a9797b7f7cbf15f967920d8129080
Matias Verdier <verdier.matias@gmail.com>
2017-03-19 21:52:00 -0300

    Split vendor modules and add csrfToken

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,13 @@
+/* global window */
 import Vue from 'vue';
 import axios from 'axios';
 
+axios.defaults.headers.common = {
+  'X-CSRF-TOKEN': window.Laravel.csrfToken,
+  'X-Requested-With': 'XMLHttpRequest',
+};
+
+/* eslint no-new: "off" */
 new Vue({
-    el: '#app'
+  el: '#app',
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+      <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,600">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+      <!-- Fonts -->
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,600">
+      <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
-        <div id="app"></div>
-        
-        <script src="{{ mix('js/app.js') }}"></script>
+      <div id="app"></div>
+      
+      <script>
+        window.Laravel = {!! json_encode([
+          'csrfToken' => csrf_token(),
+        ]) !!};
+      </script>
+      
+      <script src="{{ mix('js/manifest.js') }}"></script>
+      <script src="{{ mix('js/vendor.js') }}"></script>
+      <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
