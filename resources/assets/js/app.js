64d20ab0931a0acd21579db4dd84cc3aea488099
Matias Verdier <verdier.matias@gmail.com>
2017-03-28 02:44:19 -0300

    Add vue-router and basic setup

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,13 +1,25 @@
 /* global window */
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import axios from 'axios';
+import App from './App.vue';
+import routes from './routes';
+
+Vue.use(VueRouter);
 
 axios.defaults.headers.common = {
   'X-CSRF-TOKEN': window.Laravel.csrfToken,
   'X-Requested-With': 'XMLHttpRequest',
 };
 
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
 /* eslint no-new: "off" */
 new Vue({
   el: '#app',
+  router,
+  render: h => h(App),
 });
