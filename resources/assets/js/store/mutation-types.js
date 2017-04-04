02089363a0d14479abd025fa39494400b573ce37
Matias Verdier <verdier.matias@gmail.com>
2017-04-04 00:06:14 -0300

    add mutation types for get user data

--- resources/assets/js/store/mutation-types.js
+++ resources/assets/js/store/mutation-types.js
@@ -2,3 +2,5 @@ export const LOGIN = 'LOGIN';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGOUT = 'LOGOUT';
+export const GET_USER = 'GET_USER';
+export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
