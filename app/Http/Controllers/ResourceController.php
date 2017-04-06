98ea402fca5be945947797fec2954c5e6ea5bd2c
Matias Verdier <verdier.matias@gmail.com>
2017-04-05 21:54:59 -0300

    fix indentation, delete not used functions and implement store function

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -4,82 +4,85 @@ namespace App\Http\Controllers;
 
 use App\Resource;
 use Illuminate\Http\Request;
+use JWTAuth;
 
 class ResourceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return Resource::all();
-    }
+  
+  public function __construct()
+  {
+    $this->middleware('jwt.auth');
+  }
+  
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return Resource::all();
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store()
+  {
+    $data = request(['title', 'type', 'content']);
+    
+    $this->validate(request(), [
+      'title' => 'required',
+      'type' => 'required',
+      'content' => 'required'
+    ]);
+    
+    $user = JWTAuth::parseToken()->authenticate();
+    
+    $resource = Resource::create([
+        'title' => $data['title'],
+        'type' => $data['type'],
+        'description' => $data['content'],
+        'user_id' => $user['id'],
+    ]);
+    
+    return $resource;
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Resource  $resource
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Resource $resource)
+  {
+    return $resource;
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Resource  $resource
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Resource $resource)
-    {
-        return $resource;
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Resource  $resource
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Resource $resource)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Resource  $resource
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Resource $resource)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Resource  $resource
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Resource $resource)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Resource  $resource
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Resource $resource)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Resource  $resource
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Resource $resource)
+  {
+      //
+  }
 }
