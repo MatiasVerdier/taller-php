158a5c5ce93a6513fd84bd276f322f93b9be4c25
Matias Verdier <verdier.matias@gmail.com>
2017-04-09 19:10:18 -0300

    add iview UI

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,16 @@
 /* global window */
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import iView from 'iview';
+import locale from 'iview/src/locale/lang/en-US';
+import 'iview/dist/styles/iview.css';
 import axios from 'axios';
 import App from './App.vue';
 import routes from './routes';
 import store from './store';
 
 Vue.use(VueRouter);
+Vue.use(iView, { locale });
 
 axios.defaults.headers.common = {
   'X-CSRF-TOKEN': window.Laravel.csrfToken,
@@ -16,6 +20,16 @@ axios.defaults.headers.common = {
 const router = new VueRouter({
   mode: 'history',
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start();
+  next();
+});
+
+router.afterEach((to, from, next) => { // eslint-disable-line
+  iView.LoadingBar.finish();
+  window.scrollTo(0, 0);
 });
 
 /* eslint no-new: "off" */
