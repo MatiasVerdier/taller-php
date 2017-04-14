38767de2bce61c44dff271f5a8c8032dd221254b
Matias Verdier <verdier.matias@gmail.com>
2017-04-14 20:43:51 -0300

    use axios interceptor to set token in every request

--- resources/assets/js/api.js
+++ resources/assets/js/api.js
@@ -8,18 +8,20 @@ axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest',
 };
 
+// Add Authorization header to all requests
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`; // eslint-disable-line
+  return config;
+}, error => Promise.reject(error));
+
 // Authentication
 const login = data => axios.post('/login', data);
 const register = data => axios.post('/register', data);
 const logout = () => axios.post('/logout');
-const getUser = () => axios.get('/me', {
-  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-});
+const getUser = () => axios.get('/me');
 
 // Resources
-const addResource = data => axios.post('/resources', data, {
-  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-});
+const addResource = data => axios.post('/resources', data);
 
 export default {
   login,
