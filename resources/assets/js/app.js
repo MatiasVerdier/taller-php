63ffc3274df43062394436e3776a826ee7f75f96
Matias Verdier <verdier.matias@gmail.com>
2017-04-15 20:28:35 -0300

    change iview for element-ui

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
-import iView from 'iview';
-import locale from 'iview/src/locale/lang/en-US';
-import 'iview/dist/styles/iview.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/es';
+import 'element-ui/lib/theme-default/index.css';
+import 'vue-awesome/icons';
+import Icon from 'vue-awesome/components/Icon.vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
 
-Vue.use(iView, { locale });
+Vue.use(ElementUI, { locale });
+Vue.use(VueAxios, axios);
+Vue.component('icon', Icon);
 
 /* eslint no-new: "off" */
 new Vue({
--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import iView from 'iview';
 import routes from './routes';
 
 Vue.use(VueRouter);
@@ -8,18 +7,6 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: 'history',
   routes,
-});
-
-// Start loading bar before each route
-router.beforeEach((to, from, next) => {
-  iView.LoadingBar.start();
-  next();
-});
-
-// Finish loading bar after each route
-router.afterEach((to, from, next) => { // eslint-disable-line
-  iView.LoadingBar.finish();
-  window.scrollTo(0, 0);
 });
 
 router.beforeEach((to, from, next) => {
