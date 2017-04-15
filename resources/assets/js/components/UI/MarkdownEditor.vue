636a4eda772457983640303c593b2bb02fbfcac1
Matias Verdier <verdier.matias@gmail.com>
2017-04-15 01:14:55 -0300

    install simpleMDE and create MarkdownEditor component

---
+++ resources/assets/js/components/UI/MarkdownEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <textarea ref="area"></textarea>
+</template>
+
+<script>
+import Simplemde from 'simplemde';
+
+export default {
+  props: ['value'],
+  mounted() {
+    this.mde = new Simplemde({ element: this.$refs.area });
+    this.mde.value(this.value);
+    this.mde.codemirror.on('change', () => {
+      this.$emit('input', this.mde.value());
+    });
+  },
+  beforeDestroy() {
+    this.mde.toTextArea();
+  },
+};
+</script>
+
+<style lang="css">
+@import 'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css';
+</style>
