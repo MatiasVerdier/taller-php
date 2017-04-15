181c55382c37c4da6d503c68bf4cf87b862b6d91
Matias Verdier <verdier.matias@gmail.com>
2017-04-15 20:29:24 -0300

    change route

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -6,7 +6,7 @@ import Dashboard from './components/Dashboard.vue';
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/explore', name: 'explore', component: Explore },
-  { path: '/authenticate', name: 'login', component: Authenticate },
+  { path: '/login', name: 'login', component: Authenticate },
   { path: '/dashboard', name: 'dashboard', component: Dashboard, meta: { requireAuth: true } },
 ];
 
