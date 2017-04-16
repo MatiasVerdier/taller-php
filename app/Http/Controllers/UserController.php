9cdb28bfbfc583b04e1d753842723aaa35c7ad13
Matias Verdier <verdier.matias@gmail.com>
2017-04-16 02:11:15 -0300

    Create user controller and list resources method

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+  public function __construct()
+  {
+    $this->middleware('jwt.auth');
+  }
+  
+  
+  /**
+   * Display a listing of the resource for a particular user.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function resources(User $user) {
+    return $user->resources()->with('owner')->get();
+  }
+}
