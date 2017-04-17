1f79b782666f5ee548ad380b19ed2363b846171c
Matias Verdier <verdier.matias@gmail.com>
2017-04-17 00:18:14 -0300

    create dashboard child route to list and create resources

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -2,12 +2,30 @@ import Home from './components/Home.vue';
 import Explore from './components/Explore.vue';
 import Authenticate from './components/Authenticate.vue';
 import Dashboard from './components/dashboard/Dashboard.vue';
+import ResourceList from './components/dashboard/resources/ResourceList.vue';
+import ResourceCreate from './components/dashboard/resources/ResourceCreate.vue';
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
+  { path: '/', name: 'home', component: Home, meta: { onlyGuest: true } },
   { path: '/explore', name: 'explore', component: Explore },
   { path: '/login', name: 'login', component: Authenticate, meta: { onlyGuest: true } },
-  { path: '/dashboard', name: 'dashboard', component: Dashboard, meta: { requireAuth: true } },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard,
+    redirect: '/dashboard/resources',
+    meta: {
+      requireAuth: true,
+    },
+    children: [
+      {
+        path: 'resources', name: 'resource-list', component: ResourceList,
+      },
+      {
+        path: 'create/:type', component: ResourceCreate,
+      },
+    ],
+  },
 ];
 
 export default routes;
