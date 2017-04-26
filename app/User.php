2542cb067aebbdb18024983067d095f864d2f3fb
Matias Verdier <verdier.matias@gmail.com>
2017-04-26 01:01:48 -0300

    fix indentation

--- app/User.php
+++ app/User.php
@@ -7,33 +7,33 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'username', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'username', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token', 'created_at', 'updated_at',
-    ];
-    
-    public function resources()
-    {
-      return $this->hasMany(Resource::class);
-    }
-    
-    public function tags()
-    {
-      return $this->hasMany(Tag::class);
-    }
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token', 'created_at', 'updated_at',
+  ];
+  
+  public function resources()
+  {
+    return $this->hasMany(Resource::class);
+  }
+  
+  public function tags()
+  {
+    return $this->hasMany(Tag::class);
+  }
 }
