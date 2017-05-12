a28434080e251ddb7500eb43120fa511111c9975
Matias Verdier <verdier.matias@gmail.com>
2017-05-12 01:06:22 -0300

    display content in ResourceShow component

--- resources/assets/js/components/dashboard/resources/ResourceShow.vue
+++ resources/assets/js/components/dashboard/resources/ResourceShow.vue
@@ -1,14 +1,32 @@
 <template lang="html">
   <div class="resource-details">
-    <router-link to="/dashboard">Volver</router-link>
+    <div class="content" v-if="!isLoading && currentResource">
+      <router-link to="/dashboard">Volver</router-link>
+      
+      <h1 class="title">{{ currentResource.title }}</h1>
+      
+      <div class="body">
+        <template v-if="currentResource.type === 'LINK'">
+          {{ currentResource.link }}
+        </template>
+        
+        <template v-else-if="currentResource.type === 'MARKDOWN'">
+          <markdown-editor :value="currentResource.markdown" :isEditing="false"></markdown-editor>
+        </template>
+        
+        <template v-else="currentResource.type === 'CODE'">
+          {{ currentResource.code }}
+        </template>
+      </div>
+    </div>
     
-    <h1 class="title">{{ currentResource.title }}</h1>
-    
+    <div v-else class="loading" v-loading="isLoading"></div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import MarkdownEditor from '../../UI/MarkdownEditor.vue';
 
 export default {
   mounted() {
@@ -20,10 +38,13 @@ export default {
     },
   },
   computed: {
-    ...mapGetters(['currentResource']),
+    ...mapGetters(['currentResource', 'isLoading']),
   },
   methods: {
     ...mapActions(['getResource']),
+  },
+  components: {
+    MarkdownEditor,
   },
 };
 </script>
@@ -34,6 +55,11 @@ export default {
   
   .title {
     font-size: 2em;
+    margin: .5em 0;
+  }
+  
+  .loading {
+    min-height: 40vh;
   }
 }
 </style>
