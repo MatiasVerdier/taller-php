795b63a3a9a4e88cccac3370b655e86ca6d4672b
ayaz <imphpdevelope@gmail.com>
2017-05-25 10:59:46 +0400

    Code Formating
    Hi, I just formatted the code in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,21 +4,17 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-
       <title>Laravel</title>
-
       <!-- Fonts -->
       <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
-    <body>
-      <div id="app"></div>
-      
+    <body> 
+      <div id="app"></div><!--end #app --> 
       <script>
         window.Laravel = {!! json_encode([
-          'csrfToken' => csrf_token(),
+        'csrfToken' => csrf_token(),
         ]) !!};
       </script>
-      
       <script src="{{ mix('js/manifest.js') }}"></script>
       <script src="{{ mix('js/vendor.js') }}"></script>
       <script src="{{ mix('js/app.js') }}"></script>
