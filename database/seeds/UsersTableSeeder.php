f3bd2cce955fdd4ab3e71ebccd6a57921ccc8d31
Matias Verdier <verdier.matias@gmail.com>
2017-05-25 02:12:43 -0300

    create resources relationship in seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -10,7 +10,30 @@ class UsersTableSeeder extends Seeder
    * @return void
    */
   public function run()
-  {
-    factory(App\User::class, 5)->create();
+  {    
+    $users = factory(App\User::class, 5)
+      ->create()
+      ->each(function($user) {
+        $links = factory(App\Resource::class, 5)->states('LINK')->make();
+        $size = $links->count();
+        
+        for ($i = 0; $i < $size; $i++) {
+          $user->resources()->save($links->pop());
+        }
+        
+        $markdown = factory(App\Resource::class, 5)->states('MARKDOWN')->make();
+        $size = $markdown->count();
+        
+        for ($i = 0; $i < $size; $i++) {
+          $user->resources()->save($markdown->pop());
+        }
+        
+        $code = factory(App\Resource::class, 5)->states('CODE')->make();
+        $size = $code->count();
+        
+        for ($i = 0; $i < $size; $i++) {
+          $user->resources()->save($code->pop());
+        }
+      });
   }
 }
