e7163c6e57bb10be1c57b2cac76fcda9772cadf1
Matias Verdier <verdier.matias@gmail.com>
2017-07-06 23:02:57 -0300

    Note guarded attributes and owner relationship

--- app/Note.php
+++ app/Note.php
@@ -6,8 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
+  
+  /**
+   * The attributes that aren't mass assignable.
+   *
+   * @var array
+   */
+  protected $guarded = [];
+  
   public function resource()
   {
     return $this->belongsTo(Resource::class);
+  }
+  
+  public function owner()
+  {
+    return $this->belongsTo(User::class);
   }
 }
